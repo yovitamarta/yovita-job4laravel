f5092c18c3ce8292229d49811864edd4c883257c
yovitamarta <yovitamartadr123@gmail.com>
2025-03-10 12:32:51 +0700

    Pesan Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+            <div class="card-header">
+                <h1>Profile Saya</h1>
+            </div>
+            <div class="card-body">
+                <img src="profile.jpg" alt="Profile Picture" width="150">
+                <h3>Yovita Marta Dilla Rahmawati</h3>
+                <p>Email: yovitamartadr123@gmail.com</p>
+                <p>Bis: Pelajar SMK Negeri 1 Sayung</p>
+                <h5>Skills</h5>
+                <ul>
+                    <li>Makan</li>
+                    <li>Nonton mukbang</li>
+                    <li>Scroll tik tok</li>
+                </ul>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
